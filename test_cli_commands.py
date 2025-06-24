a3c4ba0ec28d3768c62227e7c57f6be5b2ac5804
MarzenaTatarczak <tatarczak.m@gmail.com>
2025-06-24 17:15:21 +0200

    Update test_cli_commands.py

--- test_cli_commands.py
+++ test_cli_commands.py
@@ -1,13 +1,3 @@
-"""
-CLI Command Extensions for Flask
-"""
-import os
-from unittest import TestCase
-from unittest.mock import patch, MagicMock
-from click.testing import CliRunner
-from service.common.cli_commands import db_create
-
-
 class TestFlaskCLI(TestCase):
     """Test Flask CLI Commands"""
 
